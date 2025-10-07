b2dea513bece19e5d8769a24c8dcb0e3ab8d8218
Jaros <filip.jarocki@wp.pl>
2025-10-07 18:57:58 +0200

    Initial commit

---
+++ src/main/java/com/example/slotify_backend/SlotifyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.slotify_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlotifyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlotifyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slotify_backend/SlotifyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slotify_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotifyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
